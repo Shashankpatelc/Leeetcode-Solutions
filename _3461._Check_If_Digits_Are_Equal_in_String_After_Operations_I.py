c32449849784f0c3c76d78ec075677c5b263cbb1
Shashankpatelc <s0445443@gmail.com>
2025-10-23 20:43:47 +0530

    Leetcode Solution

---
+++ _3461._Check_If_Digits_Are_Equal_in_String_After_Operations_I.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calc(self, s: str):
+        temp = ''
+        for i in range(len(s) - 1):
+            num = (int(s[i]) + int(s[i+1])) % 10
+            temp+=str(num)
+        return temp
+
+    def hasSameDigits(self, s: str) -> bool:
+        
+        while len(s) > 2:
+            s = self.calc(s)
+
+        return s[0] == s[-1]
+        
